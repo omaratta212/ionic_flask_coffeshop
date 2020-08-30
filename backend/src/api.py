5c06d8f7117f9e09d6f89a3c31a86066d3a5e56e
Omar Atta <omaratta212@gmail.com>
2020-08-30 19:27:46 +0200

    Python spaces

--- backend/src/api.py
+++ backend/src/api.py
@@ -17,7 +17,7 @@ CORS(app)
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 ## ROUTES
 '''
@@ -132,7 +132,7 @@ def update_drink(drink_id):
 
     return jsonify({
         "success": success,
-        "drink": drink.long() if success else None
+        "drinks": [drink.long()] if success else None
     })
 
 
